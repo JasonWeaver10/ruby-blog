66b9845e56f5decaccb0c902c6d51230dd72043f
jasontweaver <jasontweaver10@gmail.com>
2020-10-26 11:27:47 -0400

    added helper method for source links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def login_helper
     if current_user.is_a?(OpenStruct)
       (link_to "Sign Up", new_user_registration_path) +
@@ -8,4 +9,14 @@ module ApplicationHelper
       link_to "Logout", destroy_user_session_path, method: :delete
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+  
+
+
 end
